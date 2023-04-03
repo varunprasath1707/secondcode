651f89450787f1544274529ac960b57df3bcd3e8
varunprasath1707 <varunprasath1707@gmail.com>
2023-04-03 18:01:53 +0530

    add file

---
+++ demo.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.util.Scanner;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		int a,b;
+		int c;
+		int d=10;
+		Scanner Scanner =new Scanner(System.in);
+		a=Scanner.nextInt();
+		b=Scanner.nextInt();
+		c=a+b;
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
