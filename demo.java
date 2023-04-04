4992f24b2b92d5fedbc79eff3e6713682d997557
varunprasath1707 <varunprasath1707@gmail.com>
2023-04-04 15:52:12 +0530

    add e&f

--- demo.java
+++ demo.java
@@ -8,14 +8,17 @@ public class demo {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
 		int a,b;
-		int c;
+		int c,f;
 		int d=10;
+		int e=13;
 		Scanner Scanner =new Scanner(System.in);
 		a=Scanner.nextInt();
 		b=Scanner.nextInt();
 		c=a+b;
+		f=d*e;
 		System.out.println(c);
 		System.out.println(d);
+		System.out.println(f);
 
 	}
 
